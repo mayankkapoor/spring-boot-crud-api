a2541d8ee32de47eaaa1addcbf00989ec811a709
Mayank Kapoor <mayankkapoormail@gmail.com>
2021-03-07 18:42:51 +0530

    Update RestApiApplicationTests and add default greeting API /hello test

--- src/test/java/com/mayankkapoor/springboot/rest/example/RestApiApplicationTests.java
+++ src/test/java/com/mayankkapoor/springboot/rest/example/RestApiApplicationTests.java
@@ -4,8 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class RestApiApplicationTests {
 
 	@Autowired
@@ -14,6 +17,18 @@ class RestApiApplicationTests {
 	@Test
 	void contextLoads() throws Exception {
 		assertThat(controller).isNotNull();
+	}
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/hello",
+				String.class)).contains("Hello World");
 	}
 
 }
